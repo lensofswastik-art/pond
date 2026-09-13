74e717a07e99c3a76d4513b361302762a75a6668
lensofswastik-art <lensofswastik@gmail.com>
2026-09-14 02:21:45 +0530

    props additions with fish rotation

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,35 @@ import Ripple from "@/components/canvasui/Ripple";
 import Algae from "@/components/canvasui/Algae";
 import Flowers from "@/components/canvasui/Flowers";
 import Lotus from "@/components/canvasui/Lotus";
+import FishTrail from "@/components/FishTrail";
 
 export default function Home() {
   return (
     <Flowers className="flex flex-1 items-center justify-center bg-cover bg-center font-sans">
       <Lotus className="flex h-full w-full items-center justify-center">
-        <Ripple className="flex h-full w-full items-center justify-center" trigger="click">
-          <Algae className="flex h-full w-full items-center justify-center">
-            <main className="flex h-full w-full items-center justify-center" style={{ backgroundImage: "url(/pond.png)" }}>
-              <span
-                className="text-6xl font-semibold text-white"
-                style={{ letterSpacing: "-3%" }}
-              >
-                pond
-              </span>
-            </main>
-          </Algae>
-        </Ripple>
+        <Lotus
+          className="flex h-full w-full items-center justify-center"
+          sources={["/lotus.png"]}
+          count={3}
+          scale={0.1}
+        >
+          <Ripple className="flex h-full w-full items-center justify-center" trigger="click">
+            <Algae className="flex h-full w-full items-center justify-center">
+              <main className="relative flex h-full w-full items-center justify-center" style={{ backgroundImage: "url(/pond.png)" }}>
+                <FishTrail />
+                <span
+                  className="text-6xl font-semibold text-white"
+                  style={{
+                    letterSpacing: "-3%",
+                    fontFamily: "var(--font-cedarville-cursive)",
+                  }}
+                >
+                  pond
+                </span>
+              </main>
+            </Algae>
+          </Ripple>
+        </Lotus>
       </Lotus>
     </Flowers>
   );
---
+++ components/FishTrail.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+export function FishTrail({ className }: { className?: string }) {
+  return (
+    <div
+      className={className}
+      style={{ position: "absolute", inset: 0, pointerEvents: "none" }}
+      aria-hidden
+    >
+      <img
+        src="/fishtrail.gif"
+        alt=""
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "30%",
+          aspectRatio: "1 / 1",
+          objectFit: "contain",
+        }}
+      />
+      <img
+        src="/fishtrail2.gif"
+        alt=""
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "38%",
+          aspectRatio: "1 / 1",
+          objectFit: "contain",
+        }}
+      />
+    </div>
+  );
+}
+
+export default FishTrail;
