5aa41627a5636a449e8fc0ba0bcc1136eb4d83fa
lensofswastik-art <lensofswastik@gmail.com>
2026-09-14 01:19:01 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Ripple from "@/components/canvasui/Ripple";
 import Algae from "@/components/canvasui/Algae";
 import Flowers from "@/components/canvasui/Flowers";
 import Lotus from "@/components/canvasui/Lotus";
-import Fish from "@/components/canvasui/Fish";
 
 export default function Home() {
   return (
@@ -10,16 +9,14 @@ export default function Home() {
       <Lotus className="flex h-full w-full items-center justify-center">
         <Ripple className="flex h-full w-full items-center justify-center" trigger="click">
           <Algae className="flex h-full w-full items-center justify-center">
-            <Fish className="flex h-full w-full items-center justify-center">
-              <main className="flex h-full w-full items-center justify-center" style={{ backgroundImage: "url(/pond.png)" }}>
-                <span
-                  className="text-6xl font-semibold text-white"
-                  style={{ letterSpacing: "-3%" }}
-                >
-                  pond
-                </span>
-              </main>
-            </Fish>
+            <main className="flex h-full w-full items-center justify-center" style={{ backgroundImage: "url(/pond.png)" }}>
+              <span
+                className="text-6xl font-semibold text-white"
+                style={{ letterSpacing: "-3%" }}
+              >
+                pond
+              </span>
+            </main>
           </Algae>
         </Ripple>
       </Lotus>
