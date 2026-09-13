6afbb6a53f6ace30da2448126ce1ed169e8c9f8d
lensofswastik-art <lensofswastik@gmail.com>
2026-09-13 22:55:46 +0530

    pond background setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Ripple from "@/components/canvasui/Ripple";
 export default function Home() {
   return (
     <Ripple
-      className="flex flex-1 items-center justify-center font-sans"
-      style={{ backgroundColor: "#3083BC" }}
+      className="flex flex-1 items-center justify-center bg-cover bg-center font-sans"
+
       trigger="click"
     >
-      <main className="flex h-full w-full items-center justify-center">
+      <main className="flex h-full w-full items-center justify-center" style={{ backgroundImage: "url(/pond.png)" }}>
         <span
           className="text-6xl font-semibold text-white"
           style={{ letterSpacing: "-3%" }}
